695b298ff494d8fb7fb7cb2e4f0748455711f7e7
Connor Todd <connortodd0304@gmail.com>
2024-10-30 20:58:21 +0000

    implemented the businesses overview page

--- app/dashboard/businesses/page.tsx
+++ app/dashboard/businesses/page.tsx
@@ -40,7 +40,7 @@ export default function Businesses() {
     <div>
       <div className="container m-auto mt-14">
         {/* TODO: Implement search functionality */}
-        <form className="flex justify-center items-center gap-6 w-[90vw] md:w-[384px] m-auto mb-5 md:min-w-[300px] max-w-[500px]">
+        <form className="flex justify-center items-center gap-6 w-[90vw] md:w-[600px] m-auto mb-5 md:min-w-[300px]">
           <label
             htmlFor="groomer_search"
             className="flex justify-start items-center gap-2 w-full"
@@ -57,42 +57,59 @@ export default function Businesses() {
           <button className="btn btn-primary min-w-24 max-w-28">Search</button>
         </form>
 
-        <div className="mt-12 flex flex-wrap gap-8 justify-center items-center pb-12">
-          {/* Business Card */}
-          {businesses.map((business: Business) => (
-            <div className="card bg-base-100 w-[90vw] md:w-96 shadow-xl">
-              <figure className="h-40">
-                <img src={business?.header_image} alt={business?.name} />
-              </figure>
-              <div className="card-body">
-                <p className="text-md font-bold capitalize">{business?.name}</p>
-                <p className="text-xs">
-                  <span className="capitalize">
-                    {`${
-                      business.address_line_1 && `${business.address_line_1},`
-                    }`}
-                  </span>
-                  <span className="capitalize">
-                    {` ${
-                      business.address_line_2 && `${business.address_line_2},`
-                    }`}
-                  </span>
-                  <span className="capitalize">
-                    {` ${business.town_or_city && `${business.town_or_city},`}`}
-                  </span>
-                  <span className="capitalize">
-                    {` ${business.county && `${business.county},`}`}
-                  </span>
-                  <span className="uppercase">
-                    {` ${business.postcode && `${business.postcode}`}`}
-                  </span>
-                </p>
-                {/* <div className="card-actions justify-end">
+        <div className="max-w-[1216px] m-auto pt-20 pb-20 flex flex-col justify-center">
+          {/* TODO: Make this dynamic */}
+          <div>
+            <h1 className="text-xl text-center xl:text-left font-bold">
+              There are 3 Groomer Shops Nearby:
+            </h1>
+            <div className="mt-8 flex flex-wrap gap-8 items-center justify-center md:justify-start pb-12">
+              {/* Business Card */}
+              {businesses.map((business: Business) => (
+                <div
+                  key={business.name}
+                  className="card bg-base-100 w-[90vw] md:w-96 shadow-xl"
+                >
+                  <figure className="h-40">
+                    <img src={business?.header_image} alt={business?.name} />
+                  </figure>
+                  <div className="card-body">
+                    <p className="text-md font-bold capitalize">
+                      {business?.name}
+                    </p>
+                    <p className="text-xs">
+                      <span className="capitalize">
+                        {`${
+                          business.address_line_1 &&
+                          `${business.address_line_1},`
+                        }`}
+                      </span>
+                      <span className="capitalize">
+                        {` ${
+                          business.address_line_2 &&
+                          `${business.address_line_2},`
+                        }`}
+                      </span>
+                      <span className="capitalize">
+                        {` ${
+                          business.town_or_city && `${business.town_or_city},`
+                        }`}
+                      </span>
+                      <span className="capitalize">
+                        {` ${business.county && `${business.county},`}`}
+                      </span>
+                      <span className="uppercase">
+                        {` ${business.postcode && `${business.postcode}`}`}
+                      </span>
+                    </p>
+                    {/* <div className="card-actions justify-end">
                   <button className="btn btn-primary">Buy Now</button>
                 </div> */}
-              </div>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </div>
